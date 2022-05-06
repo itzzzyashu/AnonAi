d00aa2d5fc95f7f6aa311052469401ebcb500383
itzzzyashu™️ <83384892+itzzzyashu@users.noreply.github.com>
2022-05-06 06:54:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ import os
 
 
 class Config():
-    NAME = "Yash" # Your first name that Anon can call you
+    NAME = "Name" # Your first name that Anon can call you
     CLASS = 10 # Your class no. without section
     AGE = 16 # Your current age without(in integer).
     
@@ -19,30 +19,30 @@ class Config():
     CNT = 'IST' # IST or UTC.
 
     # Kuki Ai Api
-    AIAPI = "2088713608-KUKIJn2aIJ2lp4"
+    AIAPI = "" # Get this from https://telegram.me/kukichatbot, for token you can ask queries at https://telegram.me/metavoidsupport.
     
     # Directories
     #    Copy paste your music directory path in music_dir.
-    music_dir = r'E:\itzzzyashu backup card\Musics\English'
+    music_dir = r''
     #    Copy paste your photos directory path in pics_dir.
-    pics_dir = r'E:\itzzzyashu backup card\Photoes\MY HEART'
+    pics_dir = r''
     #    Copy paste your videos directory path in video_dir.
-    video_dir = r'e:\itzzzyashu backup card\Photoes\Romantic'
+    video_dir = r''
     #    Copy paste your AnonAi.py directory path in jarvis_path.
-    anon_path = r'c:\Users\vee\Desktop\Programming\Python\AnonAi\AnonAi.py'
+    anon_path = r''
     #    Copy paste your hidden directory path in hidden_dir.
-    hidden_dir = 'e:\itzzzyashu backup card\Photoes\HiddenXD'
+    hidden_dir = ''
     #   Secret Codes to unlock hidden_dir.
     xsearch=[
         'tea cup',
         'tea'
         ]
-        # Copy paste your Requirements.txt path in req_path.
+        
+# Copy paste your Requirements.txt path in req_path.
+req_path = r"C:\Users\user\AnonAi\requirements.txt"
 
-req_path = r"C:\Users\vee\Desktop\Programming\Python\AnonAi\requirements.txt"
-
-# Requirements Install
-# os.system(f"""pip install -r "{req_path}" """)
+# Requirements Install, after this completes, comment below line by '#'
+os.system(f"""pip install -r "{req_path}" """)
 
 
 
